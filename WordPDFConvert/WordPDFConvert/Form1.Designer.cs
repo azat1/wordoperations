8c64ededeab28c5f02739739ab7c932fe1daf574
unknown <azamatov@bk.ru>
2020-07-01 12:09:03 +0500

    first

---
+++ WordPDFConvert/WordPDFConvert/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WordPDFConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.bPrintPage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPageNum = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Word файлы|*.DOCX|Word 97|*.doc";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(776, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать файлы...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Конвертировать в PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Выбрать папку...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // bPrintPage
+            // 
+            this.bPrintPage.Location = new System.Drawing.Point(69, 230);
+            this.bPrintPage.Name = "bPrintPage";
+            this.bPrintPage.Size = new System.Drawing.Size(172, 23);
+            this.bPrintPage.TabIndex = 2;
+            this.bPrintPage.Text = "Печать";
+            this.bPrintPage.UseVisualStyleBackColor = true;
+            this.bPrintPage.Click += new System.EventHandler(this.bPrintPage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Номер страницы";
+            // 
+            // tbPageNum
+            // 
+            this.tbPageNum.Location = new System.Drawing.Point(384, 232);
+            this.tbPageNum.Name = "tbPageNum";
+            this.tbPageNum.Size = new System.Drawing.Size(100, 20);
+            this.tbPageNum.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(401, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(232, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Удалить выбранные из списка...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.tbPageNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bPrintPage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Множественная обработка Word файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button bPrintPage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPageNum;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WordPDFConvert/WordPDFConvert/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordPDFConvert
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Office.Interop.Word.Application wapp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this)==DialogResult.OK)
+            {
+                listBox1.Items.AddRange(openFileDialog1.FileNames);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wapp = new Microsoft.Office.Interop.Word.Application();
+            
+            ConvertToPdf();
+            wapp.Quit();
+            
+        }
+
+        private void ConvertToPdf()
+        {
+            foreach (string s in listBox1.Items)
+            {
+                ConvertOneFile(s);
+            }
+        }
+
+        private void ConvertOneFile(string s)
+        {
+            string pdfname=Path.ChangeExtension(s,"PDF");
+            Object path = s;
+            Document doc =wapp.Documents.Open(ref path);
+            doc.ExportAsFixedFormat(pdfname, WdExportFormat.wdExportFormatPDF);
+            doc.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderSelect();
+        }
+
+        private void FolderSelect()
+        {
+            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                AddFiles(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void AddFiles(string selectedPath)
+        {
+            var files=Directory.GetFiles(selectedPath,"*.docx");
+            listBox1.Items.AddRange(files);
+            var dirs = Directory.GetDirectories(selectedPath);
+            foreach (string dir in dirs)
+            {
+                AddFiles(dir);
+            }
+        }
+
+        private void bPrintPage_Click(object sender, EventArgs e)
+        {
+            int pagenum = int.Parse(tbPageNum.Text);
+            PrintPage(pagenum);
+        }
+
+        private void PrintPage(int pagenum)
+        {
+            wapp = new Microsoft.Office.Interop.Word.Application();
+            foreach (string fname in listBox1.Items)
+            {
+                PrintPages(wapp, pagenum,fname);
+            }
+            wapp.Quit();
+        }
+
+        private void PrintPages(Microsoft.Office.Interop.Word.Application wapp, int pagenum,string filename)
+        {
+
+            object path = filename;
+            object pages = 3;
+            object from = pagenum.ToString();
+            object to = pagenum.ToString();
+            object miss = System.Reflection.Missing.Value;
+            Document doc = wapp.Documents.Open(ref path);
+            doc.PrintOut(Range: ref pages, From: ref from, To: ref to);
+        
+            doc.Close();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var sel=listBox1.SelectedIndices;
+            for (int i=sel.Count-1;i>=0;i--)         
+            {
+                listBox1.Items.RemoveAt(sel[i]);
+            }
+        }
+    }
+}
